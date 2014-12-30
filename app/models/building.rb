db286fe382ba389a05075bf033f86d508b108054
Anders Ramsay <andersr@gmail.com>
2014-12-30 17:09:33 -0500

    building lookup for user form

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: buildings
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#  campus     :string(255)
+#
+
 class Building < ActiveRecord::Base
   has_many :users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id             :integer          not null, primary key
+#  is_shareholder :boolean
+#  first_name     :string(255)
+#  middle_initial :string(255)
+#  last_name      :string(255)
+#  nickname       :string(255)
+#  building_id    :integer
+#  created_at     :datetime
+#  updated_at     :datetime
+#
+
 class User < ActiveRecord::Base
   belongs_to :building
 end
